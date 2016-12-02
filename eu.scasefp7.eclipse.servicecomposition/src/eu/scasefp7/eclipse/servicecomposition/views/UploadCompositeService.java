4ec89098daffcf5a2eda171dc9eaef73b21d0988
Maria Koutli <mkoutli@iti.gr>
2016-12-02 17:41:12 +0200

    New WSC server.

--- eu.scasefp7.eclipse.servicecomposition/src/eu/scasefp7/eclipse/servicecomposition/views/UploadCompositeService.java
+++ eu.scasefp7.eclipse.servicecomposition/src/eu/scasefp7/eclipse/servicecomposition/views/UploadCompositeService.java
@@ -30,11 +30,14 @@ import org.json.simple.JSONValue;
 import com.auth0.jwt.JWTSigner;
 
 public class UploadCompositeService {
-	private static final String TARGET_URL = "http://109.231.127.61:8080/SCServer/rest/file/upload/";
+	private static final String TARGET_URL = "http://109.231.122.51:8080/SCServer/rest/file/upload/";
 	private int status = 0;
-	private String CTUrl = "https://app.scasefp7.com:8000/api/proxy";
-	private static JWTSigner signer = new JWTSigner("RXs7eUv1E-qROLL7UX25_w5up6284KpJE6gygLPvQhcUSwY"); // scase_secret as parameter
-	private String scase_token = "yDjOpZaBdiVtlXp5Z6KNFhopeKpEdOJTFlWJvPiBZ7atW9Y"; // scase_token
+	/// comment out until CT is back
+//	private String CTUrl = "https://app.scasefp7.com:8000/api/proxy";
+//	private static JWTSigner signer = new JWTSigner("RXs7eUv1E-qROLL7UX25_w5up6284KpJE6gygLPvQhcUSwY"); // scase_secret
+//																										// as
+//																										// parameter
+//	private String scase_token = "yDjOpZaBdiVtlXp5Z6KNFhopeKpEdOJTFlWJvPiBZ7atW9Y"; // scase_token
 
 	/**
 	 * Builds and returns a jersey client that accepts SSL certificates.
@@ -65,15 +68,36 @@ public class UploadCompositeService {
 
 	public boolean exists(String fileName) throws Exception {
 		// check if file exists
-		Client client = getClient();
+		/// comment out until CT is back////////////////////////////////////////////
+		
+		// Client client = getClient();
+		// client.property(ClientProperties.CONNECT_TIMEOUT, 1000000);
+		// client.property(ClientProperties.READ_TIMEOUT, 1000000);
+		// WebTarget webTarget = client.target(CTUrl +
+		// "/SCServer/rest/file").path(fileName);
+		//
+		// String resString = webTarget.request().header("AUTHORIZATION",
+		// "CT-AUTH " + scase_token + ":" +
+		// createSignature()).get(String.class);
+		// System.out.println(resString);
+		// JSONObject obj = (JSONObject)
+		// JSONValue.parseWithException(resString);
+		// String responseString = (String) (obj.get("body"));
+		// System.out.println(responseString);
+		// if (responseString.equals("true")) {
+		// return true;
+		// }
+		// return false;
+		
+		/// comment out until CT is back////////////////////////////////////////////////////////////
+
+		///temporary with direct call to server
+		Client client = ClientBuilder.newClient();
 		client.property(ClientProperties.CONNECT_TIMEOUT, 1000000);
 		client.property(ClientProperties.READ_TIMEOUT, 1000000);
-		WebTarget webTarget = client.target(CTUrl + "/SCServer/rest/file").path(fileName);
+		WebTarget webTarget = client.target("http://109.231.122.51:8080/SCServer/rest/file").path(fileName);
 
-		String resString = webTarget.request().header("AUTHORIZATION", "CT-AUTH " + scase_token + ":" + createSignature()).get(String.class);
-		System.out.println(resString);
-		JSONObject obj = (JSONObject) JSONValue.parseWithException(resString);
-		String responseString = (String) (obj.get("body"));
+		String responseString = webTarget.request("text/plain").get(String.class);
 		System.out.println(responseString);
 		if (responseString.equals("true")) {
 			return true;
@@ -87,11 +111,46 @@ public class UploadCompositeService {
 	public void upload(String path, IProgressMonitor monitor) throws Exception {
 
 		// upload file
-		Client client = getClient();
-		client.register(MultiPartFeature.class);
+		/// comment out until CT is back////////////////////////////////////////////////////////////
+
+//		Client client = getClient();
+//		client.register(MultiPartFeature.class);
+//		client.property(ClientProperties.CONNECT_TIMEOUT, 1000000);
+//		client.property(ClientProperties.READ_TIMEOUT, 1000000);
+//		WebTarget webTarget = client.target(CTUrl + "/SCServer/rest/file/upload");
+//
+//		if (monitor.isCanceled()) {
+//			status = 1;
+//			return;
+//		}
+//
+//		MultiPart multiPart = new MultiPart();
+//		multiPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+//
+//		FileDataBodyPart fileDataBodyPart = new FileDataBodyPart("file", new File(path),
+//				MediaType.APPLICATION_OCTET_STREAM_TYPE);
+//
+//		multiPart.bodyPart(fileDataBodyPart);
+//
+//		Response response = webTarget.request()
+//				.header("AUTHORIZATION", "CT-AUTH " + scase_token + ":" + createSignature())
+//				.post(Entity.entity(multiPart, multiPart.getMediaType()));
+//
+//		String resString = response.readEntity(String.class);
+//		System.out.println(resString);
+//
+//		System.out.println(response.getStatus() + " " + response.getStatusInfo() + " " + response);
+//
+//		status = response.getStatus();
+		
+		/// comment out until CT is back////////////////////////////////////////////////////////////
+
+		
+		
+		Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
 		client.property(ClientProperties.CONNECT_TIMEOUT, 1000000);
 		client.property(ClientProperties.READ_TIMEOUT, 1000000);
-		WebTarget webTarget = client.target(CTUrl + "/SCServer/rest/file/upload");
+		WebTarget webTarget = client.target(TARGET_URL);
 		
 		if (monitor.isCanceled()){
 			status = 1;
@@ -107,32 +166,30 @@ public class UploadCompositeService {
 
 		multiPart.bodyPart(fileDataBodyPart);
 		
-		Response response = webTarget.request().header("AUTHORIZATION", "CT-AUTH " + scase_token + ":" + createSignature())
+		Response response = webTarget.request(MediaType.APPLICATION_JSON_TYPE)
 				.post(Entity.entity(multiPart, multiPart.getMediaType()));
 
-		String resString = response.readEntity(String.class);
-		System.out.println(resString);
-		
 		System.out.println(response.getStatus() + " " + response.getStatusInfo() + " " + response);
 		
 		status = response.getStatus();
 	}
-	
-	public int getUploadStatus(){
+
+	public int getUploadStatus() {
 		return status;
 	}
-	
+
 	/**
 	 * createSignature
+	 * 
 	 * @return signature for connection with CT
 	 */
-	private String createSignature(){
-		HashMap<String, Object> claims = new HashMap<String, Object>();
-		claims.put("token", scase_token);
-
-		// create the signature
-		String signature = signer.sign(claims);
-		return signature;
-	}
+//	private String createSignature() {
+//		HashMap<String, Object> claims = new HashMap<String, Object>();
+//		claims.put("token", scase_token);
+//
+//		// create the signature
+//		String signature = signer.sign(claims);
+//		return signature;
+//	}
 
 }
